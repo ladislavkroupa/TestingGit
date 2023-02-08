dd9f23ca8c8c4255d7eceb893e2791996baaa496
Ladislav Kroupa <ladislavkroupa@Ladislavs-MacBook-Pro.local>
2023-02-08 13:06:43 +0100

    Second Commit

---
+++ FitnessApplication/FitnessApplication/FitnessApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApplicationApp.swift
+//  FitnessApplication
+//
+//  Created by Ladislav Kroupa on 08.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
